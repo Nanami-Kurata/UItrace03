addcfc5fa834f49e0c846aabf07dea569679180f
Nanami Kurata <krtnnm20011031@gmail.com>
2021-08-12 14:41:51 +0900

    initial commit

---
+++ UITrace3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UITrace3
+//
+//  Created by 倉田七海 on 2021/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var skip: UIButton!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var viewLine: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var terms: UILabel!
+    @IBOutlet weak var policy: UILabel!
+}
+
